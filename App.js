a5a519cce05ab9ca7f0ce0c7e51c87608a614540
PauloRFOliveira <paulo.oliveira.701@ufrn.edu.br>
2022-09-27 18:44:00 -0300

    Commit com Joao

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Amor da minha Vidaaaaaaaaa</Text>
+      <Text>Tô aprendendo a usar ainda, mas consegui usar.</Text>
+      <Text>Loveu Princesa</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
