bd98da99e234fc5618b6bcd010ec308ac003a4f0
PauloRFOliveira <paulo.oliveira.701@ufrn.edu.br>
2022-10-06 18:36:37 -0300

    feature: Realizada a implementação de uma segunda tela com o acesso à partir do clique de um botão.

---
+++ screens/ChatListScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ChatListScreen(){
+    return(
+        <View>
+
+            <Text>Eu te amo muito mamãe</Text>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
