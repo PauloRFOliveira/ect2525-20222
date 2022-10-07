0939dbd9e97c81d4757e192c604eeeda0c3ba6df
PauloRFOliveira <paulo.oliveira.701@ufrn.edu.br>
2022-10-07 10:39:34 -0300

    feature: Adição da tela de chat individual ao Stack Navigator.

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import ChatListScreen from "./screens/ChatListScreen";
+import ChatScreen from "./screens/ChatScreen";
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ export default function App() {
 
                 <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
                 <Stack.Screen name="ChatListScreen" component={ChatListScreen} options={{ title: "Chat List" }}/>
+                <Stack.Screen name="ChatScreen" component={ChatScreen} options={{ title: "Chat" }}/>
 
             </Stack.Navigator>
 
